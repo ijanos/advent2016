459bcbfa93f56707940e700c6e0b25b763f52dc4
János Illés <ijanos@gmail.com>
2016-12-13 13:23:39 +0100

    rust day13 part1

---
+++ rust/day13/src/main.rs
@@ -0,0 +1,53 @@
+use std::collections::{HashMap, HashSet};
+
+type Coord = (u16, u16);
+
+fn main() {
+    let mut wall_cache = HashMap::<Coord, bool>::new();
+
+    let mut is_wall = |x, y| {
+        if let Some(&wall) = wall_cache.get(&(x,y)) {
+            return wall
+        }
+        let number = x*x + 3*x + 2*x*y + y + y*y + 1350;
+        let ones = format!("{:b}", number).chars().filter(|&c| c == '1').count();
+        let wall = ones % 2 != 0;
+        wall_cache.insert((x,y),  wall);
+        wall
+    };
+
+    let start = (1, 1);
+    let goal = (31,39);
+    let mut visited = HashSet::<Coord>::new();
+
+    let mut steps = 0;
+    let mut wave = vec![start];
+    loop {
+        steps += 1;
+        let mut next_wave = Vec::new();
+        for (x, y) in wave {
+            if !is_wall(x+1, y) && !visited.contains(&(x+1, y)) {
+                next_wave.push((x+1,y));
+            }
+            if !is_wall(x, y+1) && !visited.contains(&(x, y+1)) {
+                next_wave.push((x,y+1));
+            }
+            if x > 0 && !is_wall(x-1, y) && !visited.contains(&(x-1, y)) {
+                next_wave.push((x-1,y));
+            }
+            if y > 0 && !is_wall(x, y-1) && !visited.contains(&(x, y-1)) {
+                next_wave.push((x,y-1));
+            }
+        }
+        if next_wave.contains(&goal) {
+            println!("{}", steps);
+            break;
+        }
+
+        for &c in &next_wave {
+            visited.insert(c);
+        }
+
+        wave = next_wave
+    }
+}
