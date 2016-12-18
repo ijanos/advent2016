543de3574958c40c8b7342673da138a4f5c2a6c3
János Illés <ijanos@gmail.com>
2016-12-18 22:41:30 +0100

    rust day18 part2

--- rust/day18/src/main.rs
+++ rust/day18/src/main.rs
@@ -1,30 +1,40 @@
-const INPUT:  &'static str= ".^.^..^......^^^^^...^^^...^...^....^^.^...^.^^^^....^...^^.^^^...^^^^.^^.^.^^..^.^^^..^^^^^^.^^^..^";
+const INPUT: &'static str = ".^.^..^......^^^^^...^^^...^...^....^^.^...^.^^^^....^...^^.^^^...\
+                             ^^^^.^^.^.^^..^.^^^..^^^^^^.^^^..^";
 
 fn next_row(row: &[u8]) -> String {
     let mut next = String::with_capacity(row.len());
-    let title = |left, center, right| match (left, center,right) {
-            (b'^',b'^',b'.') => '^',
-            (b'.',b'^',b'^') => '^',
-            (b'^',b'.',b'.') => '^',
-            (b'.',b'.',b'^') => '^',
-            _ => '.'
+    let title = |left, center, right| match (left, center, right) {
+        (b'^', b'^', b'.') => '^',
+        (b'.', b'^', b'^') => '^',
+        (b'^', b'.', b'.') => '^',
+        (b'.', b'.', b'^') => '^',
+        _ => '.',
     };
     // first title
     next.push(title(b'.', row[0], row[1]));
-    for i in 1..row.len() - 1  {
-        next.push(title(row[i-1], row[i], row[i+1]));
+
+    for i in 1..row.len() - 1 {
+        next.push(title(row[i - 1], row[i], row[i + 1]));
     }
+
     // last title
-    next.push(title(row[row.len()-2], row[row.len()-2], b'.'));
+    next.push(title(row[row.len() - 2], row[row.len() - 2], b'.'));
     next
 }
 
-fn main() {
-    let mut row = INPUT.to_owned();
+fn safe_titles(first_row: &str, lines: usize) -> usize {
     let mut safe = 0;
-    for _ in 0..40 {
+    let mut row = first_row.to_owned();
+    for _ in 0..lines {
         safe += row.chars().filter(|&c| c == '.').count();
         row = next_row(row.as_bytes());
     }
-    println!("part 1: {}", safe);
+    safe
+}
+
+fn main() {
+    let part1 = safe_titles(INPUT, 40);
+    let part2 = safe_titles(INPUT, 400_000);
+    println!("part 1: {}", part1);
+    println!("part 2: {}", part2);
 }
