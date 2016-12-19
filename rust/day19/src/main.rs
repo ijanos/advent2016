71c38c9a241b9d28188e01fb5e1888b89ea26a03
János Illés <ijanos@gmail.com>
2016-12-20 00:01:04 +0100

    rust day19 part2

--- rust/day19/src/main.rs
+++ rust/day19/src/main.rs
@@ -1,5 +1,21 @@
+use std::collections::VecDeque;
+
+
 fn main() {
     // https://www.youtube.com/watch?v=uCsD3ZGzMgE
     // W(n) = 2l + 1
     println!("part 1: 1834903");
+
+    let mut elves = VecDeque::new();
+    for i in 1..3014604 {
+        elves.push_back(i);
+    }
+
+    while elves.len() > 1 {
+        let k = elves.len()/2;
+        elves.remove(k);
+        let head = elves.pop_front().unwrap();
+        elves.push_back(head);
+    }
+    println!("part 2: {}", elves[0]);
 }
