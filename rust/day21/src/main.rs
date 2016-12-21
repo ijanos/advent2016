fa404d2dfb9d84253978f808a121651c00766028
János Illés <ijanos@gmail.com>
2016-12-21 18:46:15 +0100

    rust day21 part2

--- rust/day21/src/main.rs
+++ rust/day21/src/main.rs
@@ -1,74 +1,92 @@
 #![feature(advanced_slice_patterns, slice_patterns)]
 
+extern crate permutohedron;
+
 use std::io;
 use std::io::prelude::*;
 use std::collections::VecDeque;
+use permutohedron::Heap;
 
-fn main() {
-    let mut part1: VecDeque<char> = "abcdefgh".chars().collect();
-
-
-    let stdin = io::stdin();
-    for line in stdin.lock().lines() {
-        let line = line.unwrap();
+fn scramble(input: &str, commands: &Vec<String>) -> String {
+    let mut password: VecDeque<char> = input.chars().collect();
+    for line in commands {
         let line = line.split_whitespace().collect::<Vec<_>>();
         match &line[..] {
             &["rotate", "right", x, _..] => {
                 let x: usize = x.parse().unwrap();
                 for _ in 0..x {
-                    let tmp = part1.pop_back().unwrap();
-                    part1.push_front(tmp);
+                    let tmp = password.pop_back().unwrap();
+                    password.push_front(tmp);
                 }
             }
             &["rotate", "left", x, _..] => {
                 let x: usize = x.parse().unwrap();
                 for _ in 0..x {
-                    let tmp = part1.pop_front().unwrap();
-                    part1.push_back(tmp);
+                    let tmp = password.pop_front().unwrap();
+                    password.push_back(tmp);
                 }
             }
             &["rotate", _.., x] => {
                 let x = x.chars().nth(0).unwrap();
-                let x = part1.iter().position(|&c| c == x).unwrap();
+                let x = password.iter().position(|&c| c == x).unwrap();
                 let rotate = if x >= 4 { x + 2 } else { x + 1 };
                 for _ in 0..rotate {
-                    let tmp = part1.pop_back().unwrap();
-                    part1.push_front(tmp);
+                    let tmp = password.pop_back().unwrap();
+                    password.push_front(tmp);
                 }
             }
             &["reverse", _, x, _, y] => {
                 let x: usize = x.parse().unwrap();
                 let y: usize = y.parse().unwrap();
-                let start = part1.iter().take(x).cloned().collect::<Vec<char>>();
-                let mut mid = part1.iter().skip(x).take(y - x + 1).cloned().collect::<Vec<char>>();
+                let start = password.iter().take(x).cloned().collect::<Vec<char>>();
+                let mut mid =
+                    password.iter().skip(x).take(y - x + 1).cloned().collect::<Vec<char>>();
                 mid.reverse();
-                let end = part1.iter().skip(y + 1).cloned().collect::<Vec<char>>();
-                part1.clear();
-                part1.extend(start.into_iter());
-                part1.extend(mid.into_iter());
-                part1.extend(end.into_iter());
+                let end = password.iter().skip(y + 1).cloned().collect::<Vec<char>>();
+                password.clear();
+                password.extend(start.into_iter());
+                password.extend(mid.into_iter());
+                password.extend(end.into_iter());
             }
             &["move", _, x, _.., y] => {
                 let x: usize = x.parse().unwrap();
                 let y: usize = y.parse().unwrap();
-                let c = part1.remove(x).unwrap();
-                part1.insert(y, c);
+                let c = password.remove(x).unwrap();
+                password.insert(y, c);
             }
             &["swap", "letter", x, _.., y] => {
                 let x = x.chars().nth(0).unwrap();
-                let x = part1.iter().position(|&c| c == x).unwrap();
+                let x = password.iter().position(|&c| c == x).unwrap();
                 let y = y.chars().nth(0).unwrap();
-                let y = part1.iter().position(|&c| c == y).unwrap();
-                part1.swap(x, y);
+                let y = password.iter().position(|&c| c == y).unwrap();
+                password.swap(x, y);
             }
             &["swap", "position", x, _.., y] => {
                 let x: usize = x.parse().unwrap();
                 let y: usize = y.parse().unwrap();
-                part1.swap(x, y);
+                password.swap(x, y);
             }
             _ => unreachable!(),
         }
     }
-    let part1: String = part1.into_iter().collect();
+    password.into_iter().collect()
+}
+
+fn main() {
+    let stdin = io::stdin();
+    let commands: Vec<String> = stdin.lock().lines().map(|line| line.unwrap()).collect();
+
+    let part1 = scramble("abcdefgh", &commands);
     println!("part 1: {}", part1);
+
+    let part2_scrambled = "fbgdceah";
+    let mut letters: Vec<char> = part2_scrambled.chars().collect();
+    let heap = Heap::new(&mut letters);
+    for password in heap {
+        let password: String = password.into_iter().collect();
+        if scramble(&password, &commands) == part2_scrambled {
+            println!("part 2: {}", password);
+            break;
+        }
+    }
 }
