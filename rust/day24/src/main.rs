5dd517812cb999fb2d91940304869861b34b76b7
János Illés <ijanos@gmail.com>
2016-12-30 21:26:14 +0100

    rust day 24 part 2
    (and also solving part1 properly)

--- rust/day24/src/main.rs
+++ rust/day24/src/main.rs
@@ -1,19 +1,23 @@
+extern crate permutohedron;
+
 use std::io;
 use std::io::prelude::*;
-use std::collections::HashSet;
+use std::collections::{HashMap, HashSet};
+
+use permutohedron::Heap;
 
 type Pos = (u16, u16);
 
 #[derive(Debug)]
 struct Maze {
     map: HashSet<Pos>,
-    goals: HashSet<Pos>,
+    goals: Vec<Pos>,
     start: Pos,
 }
 
 fn read_maze(lines: Vec<String>) -> Maze {
     let mut map = HashSet::new();
-    let mut goals = HashSet::new();
+    let mut goals = Vec::new();
     let mut start = (0, 0);
 
     for (y, line) in lines.iter().enumerate() {
@@ -30,7 +34,7 @@ fn read_maze(lines: Vec<String>) -> Maze {
                 }
                 n if n.is_digit(10) => {
                     map.insert(coord);
-                    goals.insert(coord);
+                    goals.push(coord);
                 }
                 _ => unreachable!(),
             }
@@ -68,30 +72,47 @@ fn find_path(start: Pos, goal: Pos, map: &HashSet<Pos>) -> u32 {
     }
 }
 
-fn solve(maze: &Maze) -> u32 {
+fn solve(maze: &Maze, part1: bool) -> u32 {
+    let mut distances: HashMap<(Pos, Pos), u32> = HashMap::new();
 
-    let mut steps = 0;
-    let mut goals = maze.goals.clone();
-    let mut start = maze.start;
-
-    while !goals.is_empty() {
-        let (nearest, s) = goals.iter()
-            .cloned()
-            .map(|g| (g, find_path(start, g, &maze.map)))
-            .min_by_key(|&(_, n)| n)
-            .unwrap();
-        goals.remove(&nearest);
-        start = nearest;
-        steps += s;
+    for (i, &g1) in maze.goals.iter().enumerate() {
+        let d = find_path(maze.start, g1, &maze.map);
+        distances.insert((maze.start, g1), d);
+        distances.insert((g1, maze.start), d);
+        for &g2 in &maze.goals[i..] {
+            let d = find_path(g1, g2, &maze.map);
+            distances.insert((g1, g2), d);
+            distances.insert((g2, g1), d);
+        }
     }
 
-    steps
+    let mut goals = maze.goals.clone();
+    let heap = Heap::new(&mut goals);
+
+    let mut path: Vec<Pos> = Vec::with_capacity(maze.goals.len() + 1);
+    let mut min = u32::max_value();
+
+    for mut g in heap {
+        path.clear();
+        path.push(maze.start);
+        path.append(&mut g);
+        if part1 {
+            path.push(maze.start);
+        }
+        let cost = path.as_slice().windows(2).map(|w| distances[&(w[0], w[1])]).sum();
+        if cost < min {
+            min = cost
+        }
+    }
+    min
 }
 
 fn main() {
     let stdin = io::stdin();
     let lines: Vec<String> = stdin.lock().lines().map(Result::unwrap).collect();
     let maze = read_maze(lines);
-    let part1 = solve(&maze);
-    println!("part1: {}", part1);
+    let part1 = solve(&maze, false);
+    let part2 = solve(&maze, true);
+    println!("part 1: {}", part1);
+    println!("part 2: {}", part2);
 }
